05405a103b6a7837db8bd2ffed8b62382c317609
alifnew8531 <139584880+alifnew8531@users.noreply.github.com>
2026-04-08 20:02:03 +0600

    GENERIC T

---
+++ MyStack_v3_Generic.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MyStack<T> {
+    private ArrayList<T> elements;
+
+    public MyStack() {
+        elements = new ArrayList<>();
+    }
+
+    public T top() {
+        return elements.get(elements.size() - 1);
+    }
+
+    public void push(T n) {
+        elements.add(n);
+    }
+
+    public void pop() {
+        elements.remove(elements.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public int getSize() {
+        return elements.size();
+    }
+}
